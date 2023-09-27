5521e6764c9509780449e11c0552f07d5d978a20
Shamim <shamimhossen3912@gmail.com>
2023-09-26 23:16:16 -0700

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+export default function Page(): React.JSX.Element {
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js!</h1>
+    </div>
+  );
+}
